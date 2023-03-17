d6a386f973507cd3a36ec8900b279db7438119be
Jerome Kithinji <60386917+jeromekithinji@users.noreply.github.com>
2023-03-17 15:37:17 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
+ * main -Program that determins the status of an input
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,10 +13,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
-	else if (n < 0)
+	}
+	else
+	{
 		printf("%d is negative\n", n);
+	}
 	return (0);
 }
