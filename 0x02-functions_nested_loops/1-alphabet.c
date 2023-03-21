7449b2df0aef112f366ba1223116762a84d0489e
jeromekithinji <kithinjijerome@gmail.com>
2023-03-21 22:46:38 +0000

    Task 2 completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	putchar('\n');
+return (0);
+}
