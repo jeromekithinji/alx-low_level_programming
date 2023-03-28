b3e3bdac0173dbd581119fb1dc6e1992f5d4412b
jeromekithinji <kithinjijerome@gmail.com>
2023-03-28 19:43:29 +0100

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ *  _puts - prints a string, followed by a new line
+ *
+ * @str: parameter to check
+ * Return: 0 on success
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n'); 
+}
