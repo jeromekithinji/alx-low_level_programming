818132dfaac1802a2a14e7154c8505d04f1d972c
jeromekithinji <kithinjijerome@gmail.com>
2023-03-29 13:12:59 +0100

    Project files added and task 1 completed

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ *
+ * @dest: first string
+ * @src: second string to add to first
+ *
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+{
+	i++;
+}
+while (src[j] != '\0')
+{
+	dest[i] = src[j];
+	j++;
+}
+dest[i] = '\0';
+return (dest);
+}
