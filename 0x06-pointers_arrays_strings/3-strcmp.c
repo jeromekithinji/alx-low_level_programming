a1300b2978fc8e76800eb3b62fef0f991b85592e
jeromekithinji <kithinjijerome@gmail.com>
2023-03-30 11:45:38 +0100

    syntax corrections

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two string
- * @s1: string to compare 
- * @s2: string to be compared
- * Return: int 
+ * _strcmp - compares two strings
+ * @s1: string to be compared
+ * @s2: string for comparison
+ * Return: difference between the two strings
  */
 
 int _strcmp(char *s1, char *s2)
@@ -12,11 +12,11 @@ int _strcmp(char *s1, char *s2)
 int i;
 
 i = 0;
-while(s1[i] != '\0' && s2[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
 	if (s1[i] != s2[i])
 	{
-		return (s1[i] -s2[i]);
+		return (s1[i] - s2[i]);
 	}
 	i++;
 }
