afb9f7071cdc3fc41d71f5223ca7cbe7129b6b94
jeromekithinji <kithinjijerome@gmail.com>
2023-04-03 23:24:35 +0100

    strspn task completed

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strbrk - seacrches a string for the first occorence of 
+ * 	     any character from a set of target characters
+ * @s: string to search in
+ * @accept: set of target characters
+ * Return: pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+}
