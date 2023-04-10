c8b7ef0730f313d4771c1b7962d7113664c3d311
jeromekithinji <kithinjijerome@gmail.com>
2023-04-10 15:28:27 +0100

    Task 4 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ *
+ * Return: 0 for success and 1 for error
+ */
+
+int main (int argc, char *argv[])
+{
+int a, b;
+
+if ((argc - 1) == 2)
+{
+	a = atoi (argv[1]);
+	b = atoi (argv[2]);
+	printf("%d\n", a * b);
+}
+else 
+{
+	printf("Error\n");
+	return (1);
+}
+return (0);
+}
