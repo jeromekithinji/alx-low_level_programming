06fc894f717c1b8fc64378672fb3b36df3e29f34
jeromekithinji <kithinjijerome@gmail.com>
2023-04-10 14:58:15 +0100

    Task 3 completed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ *
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ *
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+int count;
+
+for (count = 0; count < argc; count++)
+{
+	printf("%s\n", argv[count]);
+}
+printf("\n");
+return 0;
+}
