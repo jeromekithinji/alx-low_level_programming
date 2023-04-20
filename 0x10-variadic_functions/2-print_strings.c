65d52553ac0f18adfd43ba91efcc40e0636e9690
jeromekithinji <kithinjijerome@gmail.com>
2023-04-21 00:23:00 +0300

    Project tasks

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: pointer param
+ * @n: param
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int a;
+	char *str;
+	va_start(valist, n);
+
+	for (a = 0; a < n; a++)
+	{
+		str = va_arg(valist, char *);
+		if (str)
+			printf("%s", str);
+
+		else
+			printf("(nil)");
+
+		if (a < n - 1)
+			if (separator)
+				printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(valist);
+}
