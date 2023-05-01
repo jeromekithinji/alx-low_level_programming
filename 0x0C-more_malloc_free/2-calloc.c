943706e4f911cecf07ffb52be8ae24cbc5a9f773
Jerome Kithinji <60386917+jeromekithinji@users.noreply.github.com>
2023-05-01 22:05:11 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 
 /**
  * *strr_concat - allocates memory for an array of @nmemb elements of
- * 		@size bytes each and returns pointer to the allocated memory
+ *		@size bytes each and returns pointer to the allocated memory
  *
  * @nmemb: allocated memory for array
  * @size: size bytes
@@ -15,15 +15,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arrInt;
 	unsigned int i;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	arrInt = malloc(nmemb * size);
 	if (arrInt == NULL)
 		return (NULL);
-	
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < (nmemb * size); i++)
 		arrInt[i] = 0;
 
 	return (arrInt);
