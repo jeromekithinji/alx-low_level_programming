b6daafc684657bb1e7b844f726d6725a50084a88
jeromekithinji <kithinjijerome@gmail.com>
2023-05-01 14:49:09 +0300

    Task 1 - List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - the number of elements in the 
+ * 		given linked list, listint_t.
+ *
+ * @h: the head pointer
+ * Return: the number of elements in the linked list
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t numOfElements = 0;
+
+	while (h != NULL)
+	{
+	h = h->next;
+		numOfElements++;
+	}
+	return (numOfElements);
+}
