d418945ab071e7b4b1507b2c4b5a23829b6b9970
jeromekithinji <kithinjijerome@gmail.com>
2023-05-01 16:43:47 +0300

    Task 5 - Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 – frees a linked list of integers and sets
+ *		sets the head to null
+ *
+ * @head: a double pointer to head node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
