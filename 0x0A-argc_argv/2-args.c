4f72b0536172b5bea0f910790400b3495d294c74
Jerome Kithinji <60386917+jeromekithinji@users.noreply.github.com>
2023-05-03 00:36:23 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,12 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-int count;
+	int count;
 
-for (count = 0; count < argc; count++)
-{
-	printf("%s\n", argv[count]);
-}
-printf("\n");
-return (0);
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
+	return (0);
 }
