17d5da0003e5979bdb7c40f15326470753f47d62
Jerome Kithinji <60386917+jeromekithinji@users.noreply.github.com>
2023-05-03 00:38:32 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,20 +10,20 @@
  * Return: 0 for success and 1 for error
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int a, b;
+	int a, b;
 
-if ((argc - 1) == 2)
-{
-	a = atoi (argv[1]);
-	b = atoi (argv[2]);
-	printf("%d\n", a * b);
-}
-else 
-{
-	printf("Error\n");
-	return (1);
-}
-return (0);
+	if ((argc - 1) == 2)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }
