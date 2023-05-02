8eba08e41f0d2578d6a0a9a60e6dd5d57c8abaae
jeromekithinji <kithinjijerome@gmail.com>
2023-05-03 00:53:03 +0300

    Betty corrections

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(void) 
-{
-    printf("%s\n", __FILE__);
-    return (0);
-}
+/**
+ * main - prints the name of the file it
+ *	was compiled from
+ *
+ * Return: always 0
+*/
 
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
