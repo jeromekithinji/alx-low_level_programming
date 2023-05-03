598eea103f1fbd0edeb09acd09ffaaa2e3a64f36
Jerome Kithinji <60386917+jeromekithinji@users.noreply.github.com>
2023-05-03 01:17:17 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,18 +3,20 @@
 #include "lists.h"
 
 /**
- * free_list - Release memory allocated for a linked list
- *
- * @head: Pointer to the first node of the linked list to free
- */
+  * free_list - Frees a linked list.
+  *
+  * @head: Pointer to the first node of a linked list
+  * Return: Void
+  */
 
 void free_list(list_t *head)
 {
-	if (head)
-	{
-		free_list(head->next);
-		if (head->str)
-			free(head->str);
-		free(head);
-	}
+        list_t *currentN;
+
+        while ((currentN = head) != NULL)
+        {
+                head = head->next;
+                free(currentN->str);
+                free(currentN);
+        }
 }
